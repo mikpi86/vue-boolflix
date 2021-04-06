0de011e20f406c8dc7c6799fb15436e5c0492791
mikpi86 <piras.michela86@gmail.com>
2021-04-06 16:58:08 +0200

    stampa in console array di oggetti  di film selezionati

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 /* 
-
+Milestone 1:
+Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
+⦁	Titolo
+⦁	Titolo Originale
+⦁	Lingua
+⦁	Voto
 */
 
 var app = new Vue({
@@ -8,25 +14,28 @@ var app = new Vue({
 
   data: {
 
+    searchedMovie: ''
+
   }, // chiusura data
 
   mounted(){
- 
-    axios
-      .get('https://api.themoviedb.org/3/movie/550?api_key=cdeff8ac68baa670d6a0c8c1991bbb39')
-      .then((result) => {
-
-        // stampa di result
-        console.log(result);
-
-        // prova di stampa del titolo
-        console.log(result.data.original_title);
-
-      });
 
   }, // chisure mounted
 
   methods: {
+
+    search() {
+
+      axios
+        .get('https://api.themoviedb.org/3/search/movie?api_key=cdeff8ac68baa670d6a0c8c1991bbb39&query=' + this.searchedMovie)
+        .then((result) => {
+
+          console.log(result.data.results);
+          console.log(this.searchedMovie);
+
+        });
+
+    }
 
   } // chiusura methods
   
